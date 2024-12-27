9cba9ee6f9418f98784031f23bac200d3300410d
ganjarnop <ganjarnopandi008@gmail.com>
2024-12-27 15:34:02 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+// Menu Hamburger //
+const menuBar = document.querySelector('.ri-menu-3-line');
+const menuNav = document.querySelector('.menu')
+const bgNav = document.querySelector('.bg-nav')
+
+menuBar.addEventListener('click', () => {
+    menuNav.classList.toggle('menu-active');
+    bgNav.classList.toggle('bg-active');
+});
+
+bgNav.addEventListener('click', () => {
+    menuNav.classList.remove('menu-active');
+    bgNav.classList.remove('bg-active');
+});
